e638bcd3179f20031bd4bfd433e7d5ae755be473
toel-kamanda34 <toelokemwa@gmail.com>
2024-08-26 11:03:22 +0300

    completed test lab

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,14 @@
 // Your tests here
+import {pointsForWord} from "../utils"
+
+describe("pointsForWord", () =>{
+    it("handles uppercase and lowercase input",() =>{
+      
+      const word="tEsT";
+ 
+      const points= pointsForWord(word);
+ 
+      expect(points).toBe(7);
+ 
+    });
+ });
